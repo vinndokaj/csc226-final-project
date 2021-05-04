4cfa2402feb7f4a35dfd51f9a4345e9613b55074
Ivan Lei <ilei0893@gmail.com>
2021-05-04 00:59:46 -0400

    Changed cookeis to session

--- client/home.php
+++ client/home.php
@@ -1,1 +1,10 @@
+<?php
+
+//Sends user to home page or login page based on cookies
+  if(isset($_COOKIE['$cookie_value'] )){
+      header("Location: login.php"); 
+    }
+?>
+
+
 <div>Welcome</div>
