f819dbd01797f2427f857ec7b8fab2b65a44f09a
Ivan Lei <ilei0893@gmail.com>
2021-05-04 01:26:23 -0400

    Converted home page to conditional html based on user_email session variable. Added logout file to destroy session

--- client/home.php
+++ client/home.php
@@ -1,10 +1,54 @@
 <?php
 
-//Sends user to home page or login page based on cookies
-  if(isset($_COOKIE['$cookie_value'] )){
-      header("Location: login.php"); 
-    }
+//Start session
+session_start();
+
+//If user_email session variable exists, display homepage
+if(isset($_SESSION['user_email']) && !empty($_SESSION['user_email'])) {
+  ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Welcome!</title>
+</head>
+
+<body>
+  <div class="container">
+    <nav class="navbar navbar-light">
+      <div>
+        <a href="logout.php" class="ml-2 btn btn-danger">Logout</a>
+      </div>
+    </nav>
+
+  </div>
+
+  <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
+<?php
+}
+else{
+  header("Location: login.php");   
+}
 ?>
 
 
-<div>Welcome</div>
+<!-- TODO check for cookies or session for auto login -->
